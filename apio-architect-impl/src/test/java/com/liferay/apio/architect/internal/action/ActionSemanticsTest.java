370d7f97cf95e972f75fdf98f1ae3ff2d2302a90
Alejandro Hernández <info@alejandrohdezma.com>
2018-11-02 20:10:39 +0100

    APIO-244 Handle retrieving root collections with action semantics

--- apio-architect-impl/src/test/java/com/liferay/apio/architect/internal/action/ActionSemanticsTest.java
+++ apio-architect-impl/src/test/java/com/liferay/apio/architect/internal/action/ActionSemanticsTest.java
@@ -14,26 +14,26 @@
 
 package com.liferay.apio.architect.internal.action;
 
-import static com.liferay.apio.architect.operation.HTTPMethod.GET;
-
-import static java.lang.String.join;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
-import static org.hamcrest.core.Is.is;
-
 import com.liferay.apio.architect.internal.action.resource.Resource;
-
+import com.liferay.apio.architect.internal.annotation.Action;
 import io.vavr.CheckedFunction1;
+import io.vavr.control.Try;
+import org.immutables.value.Value.Immutable;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
-import org.immutables.value.Value.Immutable;
-
-import org.junit.Test;
+import static com.liferay.apio.architect.internal.action.ActionSemantics.toAction;
+import static com.liferay.apio.architect.operation.HTTPMethod.GET;
+import static java.lang.String.join;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.core.Is.is;
 
 /**
  * @author Alejandro Hernández
@@ -55,7 +55,7 @@ public class ActionSemanticsTest {
 		).annotatedWith(
 			_myAnnotation
 		).executeFunction(
-			list -> join("-", (List<String>)list)
+			_join
 		).build();
 
 		assertThat(actionSemantics.resource(), is(Resource.Paged.of("name")));
@@ -88,7 +88,7 @@ public class ActionSemanticsTest {
 		).returnsNothing(
 		).notAnnotated(
 		).executeFunction(
-			list -> join("-", (List<String>)list)
+			_join
 		).build();
 
 		assertThat(actionSemantics.resource(), is(Resource.Paged.of("name")));
@@ -122,7 +122,7 @@ public class ActionSemanticsTest {
 		).annotatedWith(
 			_myAnnotation
 		).executeFunction(
-			list -> join("-", (List<String>)list)
+			_join
 		).build();
 
 		assertThat(actionSemantics.resource(), is(Resource.Paged.of("name")));
@@ -155,7 +155,7 @@ public class ActionSemanticsTest {
 		).annotatedWith(
 			_myAnnotation
 		).executeFunction(
-			list -> join("-", (List<String>)list)
+			_join
 		).build();
 
 		assertThat(actionSemantics.resource(), is(Resource.Paged.of("name")));
@@ -206,12 +206,47 @@ public class ActionSemanticsTest {
 		assertThat(result, is("1-2"));
 	}
 
+	@Test
+	public void testToActionTransformsAnActionSemanticsIntoAnAction() {
+		ActionSemantics actionSemantics = ActionSemantics.ofResource(
+			Resource.Paged.of("name")
+		).name(
+			"action"
+		).method(
+			GET
+		).receivesParams(
+			String.class, Long.class
+		).returns(
+			String.class
+		).annotatedWith(
+			_myAnnotation
+		).executeFunction(
+			_join
+		).build();
+
+		Function<ActionSemantics, Action> toActionFunction = toAction(
+			__ -> Class::getSimpleName);
+
+		Action action = toActionFunction.apply(actionSemantics);
+
+		Object object = action.apply(null);
+
+		assertThat(object, is(instanceOf(Try.class)));
+
+		@SuppressWarnings("unchecked")
+		Try<String> stringTry = (Try<String>)object;
+
+		assertThat(stringTry.get(), is("String-Long"));
+	}
+
 	@Immutable(singleton = true)
 	public static @interface MyAnnotation {
 	}
 
+	@SuppressWarnings("unchecked")
 	private static final CheckedFunction1<List<?>, Object> _join = list -> join(
 		"-", (List<String>)list);
+
 	private static final MyAnnotation _myAnnotation =
 		ImmutableMyAnnotation.of();
 
